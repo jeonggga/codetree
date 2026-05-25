760cb20bbce9c7b89d37b567df2fa9608240b4b7
jeonggga <241048995+jeonggga@users.noreply.github.com>
2026-05-26 06:47:12 +0900

    [Passed] 굉장한 수 (62ms, 10MB)

---
+++ 260526/굉장한 수/amazing-number.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+let input = Number(fs.readFileSync(0).toString().trim());
+
+if ((input % 3 === 0 && input % 2 !== 0) || (input % 2 === 0 && input % 5 === 0)) {
+    console.log("true");
+} else {
+    console.log("false");
+}
